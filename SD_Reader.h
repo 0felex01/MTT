f4e31efe9f6ceff3aabc0e8589f69d51481da6f9
Boyle <stephen.boyle_cn@CMW-CWL6164.cacicorenet.com>
2026-03-12 15:08:41 -0400

    changed the sorting algo to use KickSort's Insertion sort

--- SD_Reader.h
+++ SD_Reader.h
@@ -1,3 +1,5 @@
+#include "KickSort.h"
+
 #define MAX_FILES 30
 #define MAX_ROWS 9
 
@@ -5,48 +7,6 @@ void redrawFiles(String files[MAX_FILES], int filesCount) {
     u8g2.clearBuffer();
     for (int i = 0; i < filesCount; ++i) {
         OLED_printLine(files[i], i);
-    }
-}
-
-void bubbleSort(String files[MAX_FILES], int filesCount) {
-    bool good = false;
-    unsigned int char_pos = 0;
-    bool clean = false;
-    String temp;
-
-    while (!clean) {
-        clean = true; // For exiting sort after one full clean run
-        for (int i = 0; i < (filesCount - 1); ++i) {
-            good = false; // For checking letter by letter if need to
-            char_pos = 0;
-
-            while (!good) {
-                // Same letters
-                if (files[i][char_pos] == files[i + 1][char_pos]) {
-                    if (char_pos < (files[i].length() - 1) && char_pos < (files[i + 1].length() - 1)) { // Keep cursor pos within strings' lengths
-                        ++char_pos;
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-
-                // Swap
-                if (files[i][char_pos] > files[i + 1][char_pos]) {
-                    temp = files[i + 1];
-                    files[i + 1] = files[i];
-                    files[i] = temp;
-
-                    good = true;
-                    clean = false;
-                }
-
-                // Good as is
-                if (files[i][char_pos] < files[i + 1][char_pos]) {
-                    good = true;
-                }
-            }
-        }
     }
 }
 
@@ -68,7 +28,7 @@ int getFiles(String files[MAX_ROWS]) {
     file.close();
 
     // Sort alphabetically
-    bubbleSort(files, filesCount);
+    KickSort<String>::insertionSort(files, filesCount);
 
     // Strip .srt extensions
     for (int i = 0; i < filesCount; ++i) {
